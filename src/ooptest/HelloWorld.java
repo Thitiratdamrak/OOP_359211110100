11fc455474f33535a2488088d303d6fb71465e6f
Thitiratdamrak <fonhmi0223@gmail.com>
2017-11-21 15:19:47 +0700

    Testing Add Data

--- src/ooptest/HelloWorld.java
+++ src/ooptest/HelloWorld.java
@@ -5,6 +5,12 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello world,I'm fon");
+        // Add Data
+        System.out.println("Thitirat damrak");
+        System.out.println("Information ");
+        System.out.println("Manaagement");
+        System.out.println("Ruv");
+
 
     }//main
 }
