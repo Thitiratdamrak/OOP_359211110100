2717424b8113a74bbb81238aefd458ba560d0774
thitiratdamrak <fonhmi0223@gmail.com>
2017-11-29 10:39:15 +0700

    CalGrade

--- src/OOPlab3/CalGrade.java
+++ src/OOPlab3/CalGrade.java
@@ -1,12 +1,32 @@
 //condition
-  //      score 0-49 grade F
-    //    score 50-59 grade D
-   //     score 60-69 grade C
-   //     score 80-100 grade A
-
+//        score 0-49 grade F
+//        score 50-59 grade D
+//         score 60-79 grade C
+//         score 70-89 grade B
+//         score 80-100 grade A
 package OOPlab3;
 
-public class CalGrade {
+import java.util.Scanner;
 
+public class CalGrade {
+    public static  void  main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int scor;
+        System.out.print("Enter your score: ");
+        scor = scanner.nextInt();
+//      test condition scor
+        if (scor < 49)
+            System.out.println("your grade is F");
+        else if (scor < 59)
+            System.out.println("your grade is D");
+        else  if (scor < 69)
+            System.out.println("your grade is C");
+        else if (scor < 79)
+            System.out.println("your grade is B");
+        else System.out.println("your grade is A");
+    }
 }
+
+
+
 
