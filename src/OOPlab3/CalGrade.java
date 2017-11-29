a596134a4d85ac4688fad2593836df56529add3e
thitiratdamrak <fonhmi0223@gmail.com>
2017-11-29 09:51:19 +0700

    TestCondition

---
+++ src/OOPlab3/CalGrade.java
@@ -0,0 +1,12 @@
+//condition
+  //      score 0-49 grade F
+    //    score 50-59 grade D
+   //     score 60-69 grade C
+   //     score 80-100 grade A
+
+package OOPlab3;
+
+public class CalGrade {
+
+}
+
