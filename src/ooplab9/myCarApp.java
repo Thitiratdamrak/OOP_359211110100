10489fdf89ed80cd2d7ce29e2753943cde353041
Thitiratdamrak <Fonhmi0223@gmail.com>
2018-02-08 21:09:23 +0700

    myCarApp

--- src/ooplab9/myCarApp.java
+++ src/ooplab9/myCarApp.java
@@ -1,4 +1,17 @@
 package ooplab9;
 
+import javax.swing.text.Style;
+
 public class myCarApp {
-}
+    public static void main(String[] args) {
+        Engine engine = new Engine("1500 cc",
+                "V-TEC V4");
+        car car = new car("Honda", "black",
+                "City", engine);
+        System.out.println(car.toString());
+
+        car car1 = new car("Toyota", "Red",
+                "Altis", engine);
+        System.out.println(car1.toString());
+    }//main
+}//class
